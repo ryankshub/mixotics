3180461956d4d27a999d69d012e48b9e8fd27955
Ryan King-Shepard <ryanking-shepard2022@u.northwestern.edu>
2021-11-23 18:37:31 -0600

    Added declaration of parse_menu_xml function

---
+++ src/menu_utility.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+#Python file for various helper functions for processing menus
+#RKS
+
+# Project imports
+from enum_defs import InstrEnum, StoreRoomEnum
+from menu_item import MenuItem
+
+# Python imports
+import xml
+
+# 3rd-party imports
+
+###
+def parse_menu_xml(menu_file):
+    rtn_dict = {}
+    return rtn_dict
