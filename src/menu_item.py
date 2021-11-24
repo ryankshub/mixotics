9082ff26335921a8a105b2d66a030f7fec9d2c88
Ryan King-Shepard <ryanking-shepard2022@u.northwestern.edu>
2021-11-23 18:31:22 -0600

    Added first draft of menuitem class, the representation of the different drinkes the robot can make

---
+++ src/menu_item.py
@@ -0,0 +1,84 @@
+#!usr/bin/env python3
+#Python file contain definition for MenuItem
+#RKS
+
+# Project imports
+from enum_defs import InstrEnum, StoreRoomEnum
+# Python imports
+
+# 3rd party imports
+
+class MenuItem:
+    """
+    Representation of an menu item the Franka robot can make.
+    """
+    def __init__(self, name, ingredients, instructions):
+        """
+        Constructor of the MenuItem class
+
+        Args:
+            str name:
+                ~name of the menu item
+            list ingredients:
+                ~list of ingredients needed to make the menu items. The list 
+                should contain enum from the StoreRoomEnum
+            list instructions:
+                ~Recipe of the menu item. The recipe should contain 
+                enums from the InstrEnum
+        
+        Raise:
+            RuntimeError:
+                Raises a Runtime Error if an ill-defined ingredient or 
+                instruction is found.
+        """
+        self._name = name
+        self._ingredients = ingredients
+        self._recipe = instructions
+
+        ## Check ingredients
+        false_ingredients = []
+        for ingredient in self._ingredients:
+            if not isinstance(ingredient, StoreRoomEnum):
+                false_ingredients.append(ingredient)
+        if len(false_ingredients) != 0:
+            err_string = f"Unknown ingredients found: {false_ingredients}"
+            raise RuntimeError(err_string)
+
+
+        ## Check instructions
+        false_instructions = []
+        for instruction in self._recipe:
+            if not isinstance(instruction, InstrEnum):
+                false_instructions.append(instruction)
+        if len(false_instructions) != 0:
+            err_string = f"Unknown instructions found: {false_instructions}"
+            raise RuntimeError(err_string)
+
+    @property
+    def name(self):
+        """
+        Get name of the item
+        """
+        return self._name
+
+    @name.setter
+    def name(self):
+        """
+        Userbase cannot change the name of the menu item
+        """
+        print(f"WARN: Menu item {self._name} cannot have it's name changed")
+        pass
+    
+    @property
+    def ingredients(self):
+        """
+        Get the ingredients for the menu item
+        """
+        return self._ingredients.copy()
+
+    @property
+    def recipe(self):
+        """
+        Get the recipe for the menu item
+        """
+        return self._instructions.copy()
